f8c2ffe8d0c863658fcb9a82a3d0d1634adf44cb
Nicolas Liu <nicolasliu18@gmail.com>
2023-09-22 15:49:30 -0400

    main

--- c_wcmain.c
+++ c_wcmain.c
@@ -14,7 +14,6 @@ int main(int argc, char **argv) {
   const unsigned char *best_word = (const unsigned char *) "";
   uint32_t best_word_count = 0;
 
-  // TODO: implement
 
   // Initialize hash table
   struct WordEntry *hashtable[HASHTABLE_SIZE];
@@ -27,6 +26,7 @@ int main(int argc, char **argv) {
   }
 
   unsigned char curr_word[MAX_WORDLEN + 1];
+
   while (wc_readnext(fp, curr_word)) {
     //   increase total word count by 1
     total_words++;
@@ -38,22 +38,36 @@ int main(int argc, char **argv) {
     wc_trim_non_alpha(curr_word);
 
     //   use wc_dict_find_or_insert to find or insert the word in the hash table
-    int inserted = 0;
-    struct WordEntrywc_dict_find_or_insert(hashtable, curr_word, &inserted);
-    if (inserted == 1) {
-      unique_words++;
-    }
+    struct WordEntry *word = wc_dict_find_or_insert(hashtable, HASHTABLE_SIZE, curr_word);
 
     // increment the WordEntry's count
+    word->count++;
+  }
 
-  // }
+  // Find best word, its count, and number of unique words by traversing entire hash table
+  for (int i = 0; i < HASHTABLE_SIZE; i++) {
+    struct WordEntry *cursor = hashtable[i];
+    while (cursor != NULL) {
+      unique_words++;
+      if (cursor->count > best_word_count) {
+        best_word = cursor->word;
+        best_word_count = cursor->count;
+      }
+      cursor = cursor->next;
+    }
+  }
 
   printf("Total words read: %u\n", (unsigned int) total_words);
   printf("Unique words read: %u\n", (unsigned int) unique_words);
   printf("Most frequent word: %s (%u)\n", (const char *) best_word, best_word_count);
 
   // TODO: make sure file is closed (if one was opened)
+  fclose(fp);
   // TODO: make sure memory is freed
+  for (int i = 0; i < HASHTABLE_SIZE; i++) {
+    struct WordEntry *cursor = hashtable[i];
+    wc_free_chain(cursor);
+  }
 
   return 0;
 }
