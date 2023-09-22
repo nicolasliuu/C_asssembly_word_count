a7655853f2bc7560d743705c3dd6040f53db30f7
Nicolas Liu <nicolasliu18@gmail.com>
2023-09-22 15:14:59 -0400

    main

--- c_wcmain.c
+++ c_wcmain.c
@@ -16,6 +16,28 @@ int main(int argc, char **argv) {
 
   // TODO: implement
 
+  // Initialize hash table
+  struct WordEntry *hashtable[HASHTABLE_SIZE];
+
+  // Open input file
+  FILE *fp = fopen(argv[1], "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Error: failed to open file '%s'\n", argv[1]);
+    return 1;
+  }
+
+  //   while ( next word is read successfully using wc_readnext ) {
+  //   increase total word count by 1
+
+  //   use wc_tolower to convert word to lower case
+
+  //   use wc_trim_non_alpha to remove non-alphabetic characters at end of word
+
+  //   use wc_dict_find_or_insert to find or insert the word in the hash table
+
+  //   increment the WordEntry's count
+  // }
+
   printf("Total words read: %u\n", (unsigned int) total_words);
   printf("Unique words read: %u\n", (unsigned int) unique_words);
   printf("Most frequent word: %s (%u)\n", (const char *) best_word, best_word_count);
